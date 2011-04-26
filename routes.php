a89bd3955d29dd276a1c8f5b3b54df0c05b51031
James Cleveland <jamescleveland@gmail.com>
2011-04-26 14:43:33 +0000

    cache enable

--- routes.php
+++ routes.php
@@ -1,7 +1,7 @@
 <?php // Routeses
 
 $routes = array(
-    '^([0-9]+)(.*)$' => 'Page:method=display_article;article_id=$1',
+    '^([0-9]+)(.*)$' => 'Page:method=display_article;article_id=$1;__cache__=on',
     '^admin/login' => 'Admin:method=login',
     '^admin/logout' => 'Admin:method=logout',
     '^admin/set-article-active' => 'Admin:method=set_article_active',
@@ -9,6 +9,6 @@ $routes = array(
     '^admin/delete-article/(.*)$' => 'Admin:method=delete_article;article_id=$1',
     '^admin/new-article$' => 'Admin:method=edit_article',
     '^admin' => 'Admin',
-    '^$' => 'Page:method=index'
+    '^$' => 'Page:method=index;__cache__=on'
 );
 
